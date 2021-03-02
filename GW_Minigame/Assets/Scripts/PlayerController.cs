1cf2883f343458fa8bb07cae2e596b8bee4f954f
ZePumpkinKing <79880943+ZePumpkinKing@users.noreply.github.com>
2021-03-02 12:59:20 -0500

    Added player animations

--- GW_Minigame/Assets/Scripts/PlayerController.cs
+++ GW_Minigame/Assets/Scripts/PlayerController.cs
@@ -7,13 +7,23 @@ public class PlayerController : MonoBehaviour
     // Variable declarations
     private float horizontalInput;
     private float verticalInput;
+
     public float speed = 20.0f;
     public float xRange = 15;
     public float zRange = 15;
+
     public GameObject projectilePrefab;
+
     private bool cooldown = true;
+
     private int downTime = 0;
+
     private bool gameOver = false;
+
+    public GameObject ship;
+    public GameObject exhaust;
+
+    private int exhaustRotation = 1;
 
     // Start is called before the first frame update
     void Start() {
@@ -58,6 +68,34 @@ public class PlayerController : MonoBehaviour
             Instantiate(projectilePrefab, transform.position + new Vector3(0, 0, 10), projectilePrefab.transform.rotation);
             cooldown = true;
         }
+
+        // Exhaust Animation
+        if (!Input.GetKey(KeyCode.S)) {
+            if (exhaustRotation == 1) {
+                exhaust.transform.localScale = new Vector3(0.03f, 0.03f, 0.1f);
+            } else if (exhaustRotation == 2) {
+                exhaust.transform.localScale = new Vector3(0.03f, 0.03f, 0.15f);
+            } else if (exhaustRotation == 3) {
+                exhaust.transform.localScale = new Vector3(0.03f, 0.03f, 0.2f);
+            } else if (exhaustRotation == 4) {
+                exhaust.transform.localScale = new Vector3(0.03f, 0.03f, 0.15f);
+            }
+            if (exhaustRotation == 4) {
+                exhaustRotation = 1;
+            } else {
+                exhaustRotation++;
+            }
+        } else{
+            exhaust.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+
+        // Turn Banking
+        if (verticalInput < 0) {
+            ship.transform.rotation = Quaternion.Euler(verticalInput * 10, 0, horizontalInput * -20);
+        } else {
+            ship.transform.rotation = Quaternion.Euler(0, 0, horizontalInput * -20);
+        }
+        
 
     }
 }
